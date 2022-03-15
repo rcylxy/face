b7412a014c53c28fd08f0eed66788d3ddf4fdaad
rcylxy <islixiaoyang2021@163.com>
2022-03-15 08:56:28 +0800

    add takephotos

---
+++ takephotos/CMakeFiles/takephotos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/takephotos.dir/takephotos.cpp.o"
+  "takephotos"
+  "takephotos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/takephotos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
