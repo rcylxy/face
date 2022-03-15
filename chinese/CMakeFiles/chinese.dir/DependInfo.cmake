178fc9a75028572810e03db9cde7e796596e7d05
rcylxy <islixiaoyang2021@163.com>
2022-03-15 09:00:39 +0800

    hello

---
+++ chinese/CMakeFiles/chinese.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lxy/test/chinese/CvxText.cpp" "/home/lxy/test/chinese/CMakeFiles/chinese.dir/CvxText.cpp.o"
+  "/home/lxy/test/chinese/main.cpp" "/home/lxy/test/chinese/CMakeFiles/chinese.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/freetype2"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ chinese/CMakeFiles/chinese.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chinese.dir/CvxText.cpp.o"
+  "CMakeFiles/chinese.dir/main.cpp.o"
+  "chinese"
+  "chinese.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chinese.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
